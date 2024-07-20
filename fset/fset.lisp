dcad65baf955ac1c7f10cbbe1b67094f9452561a
Colin Woodbury <colin@fosskers.ca>
2024-07-20 12:22:41 +0900

    feat(fset): add `defmethod` definitions

--- fset/fset.lisp
+++ fset/fset.lisp
@@ -3,9 +3,105 @@
   (:shadow #:set #:map)
   (:local-nicknames (#:t #:transducers)
                     (#:s #:fset))
+  (:export #:set #:map #:seq #:bag)
   (:documentation "Fset extensions for Transducers."))
 
 (in-package :transducers/fset)
+
+;; --- Transducers --- ;;
+
+(defmethod t:transduce (xform f (source s:set))
+  (set-transduce xform f source))
+
+(defmethod t:transduce (xform f (source s:map))
+  (map-transduce xform f source))
+
+(defmethod t:transduce (xform f (source s:seq))
+  (seq-transduce xform f source))
+
+(defmethod t:transduce (xform f (source s:bag))
+  (bag-transduce xform f source))
+
+(defun set-transduce (xform f coll)
+  (let* ((init   (funcall f))
+         (xf     (funcall xform f))
+         (result (set-reduce xf init coll)))
+    (funcall xf result)))
+
+(defun set-reduce (f identity set)
+  (let ((iter (s:iterator set)))
+    (labels ((recurse (acc)
+               (if (funcall iter :done?)
+                   acc
+                   (let ((acc (t::safe-call f acc (funcall iter :get))))
+                     (if (t:reduced-p acc)
+                         (t:reduced-val acc)
+                         (recurse acc))))))
+      (recurse identity))))
+
+#+nil
+(t:transduce #'t:pass #'t:cons (s:set 1 2 3 1))
+
+(defun map-transduce (xform f coll)
+  (let* ((init   (funcall f))
+         (xf     (funcall xform f))
+         (result (map-reduce xf init coll)))
+    (funcall xf result)))
+
+(defun map-reduce (f identity set)
+  (let ((iter (s:iterator set)))
+    (labels ((recurse (acc)
+               (if (funcall iter :done?)
+                   acc
+                   (multiple-value-bind (key val) (funcall iter :get)
+                     (let ((acc (t::safe-call f acc (cl:cons key val))))
+                       (if (t:reduced-p acc)
+                           (t:reduced-val acc)
+                           (recurse acc)))))))
+      (recurse identity))))
+
+#+nil
+(t:transduce (t:map #'car) #'t:cons (s:map (:a 1) (:b 2) (:c 3)))
+
+(defun seq-transduce (xform f coll)
+  (let* ((init   (funcall f))
+         (xf     (funcall xform f))
+         (result (seq-reduce xf init coll)))
+    (funcall xf result)))
+
+(defun seq-reduce (f identity set)
+  (let ((iter (s:iterator set)))
+    (labels ((recurse (acc)
+               (if (funcall iter :done?)
+                   acc
+                   (let ((acc (t::safe-call f acc (funcall iter :get))))
+                     (if (t:reduced-p acc)
+                         (t:reduced-val acc)
+                         (recurse acc))))))
+      (recurse identity))))
+
+#+nil
+(t:transduce #'t:pass #'t:cons (s:seq 1 2 3 1))
+
+(defun bag-transduce (xform f coll)
+  (let* ((init   (funcall f))
+         (xf     (funcall xform f))
+         (result (bag-reduce xf init coll)))
+    (funcall xf result)))
+
+(defun bag-reduce (f identity set)
+  (let ((iter (s:iterator set)))
+    (labels ((recurse (acc)
+               (if (funcall iter :done?)
+                   acc
+                   (let ((acc (t::safe-call f acc (funcall iter :get))))
+                     (if (t:reduced-p acc)
+                         (t:reduced-val acc)
+                         (recurse acc))))))
+      (recurse identity))))
+
+#+nil
+(t:transduce #'t:pass #'t:cons (s:bag 1 2 3 1))
 
 ;; --- Reducers --- ;;
 
